d2206409a3b4f738e6c02e418fb302702dc42333
Ajay Sai <mac@Ajays-iMAC.local>
2025-05-27 20:37:54 +0530

    Resume Updated

--- src/components/layout/PrimaryButton.jsx
+++ src/components/layout/PrimaryButton.jsx
@@ -2,7 +2,7 @@ import styles from "@/styles/PrimaryButton.module.css";
 
 const PrimaryButton = () => {
   return (
-    <a className={styles.Button}>
+    <a className={styles.Button} href="https://drive.google.com/file/d/1FvBnt5XxOM2INyHzRgkYxLbyXBUNqXQj/view?usp=sharing" target="_blank" rel="noopener noreferrer">
       Download resume
     </a>
   );
